351df23ffaac00128ba8575d00dd6c160b7c3cb6
dasaczzz <118865516+dasaczzz@users.noreply.github.com>
2025-08-11 01:05:06 -0500

    feat: adding the exception handlers for bad post requests

--- src/main/java/com/apiweb/socialbooks/Controller/GlobalExceptionHandler.java
+++ src/main/java/com/apiweb/socialbooks/Controller/GlobalExceptionHandler.java
@@ -4,6 +4,8 @@ import com.apiweb.socialbooks.Exception.ResourceNotFound;
 import com.apiweb.socialbooks.Lib.BaseResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.HttpMediaTypeNotSupportedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
@@ -22,5 +24,25 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
     }
 
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<BaseResponse> handleHttpMessageNotReadableException(WebRequest request) {
+        BaseResponse errorResponse = new BaseResponse(
+            HttpStatus.BAD_REQUEST.value(),
+            "Required request JSON body is missing",
+            BaseResponse.getMethodFromRequest(request),
+            BaseResponse.getPathFromRequest(request)
+        );
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+    }
 
+    @ExceptionHandler(HttpMediaTypeNotSupportedException.class)
+    public ResponseEntity<BaseResponse> handleHttpMediaTypeNotSupportedException(HttpMediaTypeNotSupportedException e, WebRequest request) {
+        BaseResponse errorDetails = new BaseResponse(
+                HttpStatus.UNSUPPORTED_MEDIA_TYPE.value(),
+                "Bad Content type. Not supported",
+                BaseResponse.getMethodFromRequest(request),
+                BaseResponse.getPathFromRequest(request)
+        );
+        return new ResponseEntity<>(errorDetails, HttpStatus.UNSUPPORTED_MEDIA_TYPE);
+    }
 }
