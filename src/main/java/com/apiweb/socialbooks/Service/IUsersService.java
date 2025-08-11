e8006b8b976429b8f807672f56544f704b20da69
dasaczzz <118865516+dasaczzz@users.noreply.github.com>
2025-08-11 00:44:41 -0500

    fix: remove delete user from friendlists from service to work private

--- src/main/java/com/apiweb/socialbooks/Service/IUsersService.java
+++ src/main/java/com/apiweb/socialbooks/Service/IUsersService.java
@@ -2,11 +2,9 @@ package com.apiweb.socialbooks.Service;
 
 import com.apiweb.socialbooks.Model.UserProfilesView;
 import com.apiweb.socialbooks.Model.UsersModel;
-import org.bson.types.ObjectId;
 
 import java.util.List;
 
 public interface IUsersService extends BaseService<UsersModel> {
     List<UserProfilesView> getUserProfiles();
-    String deleteUserFromFriendLists(ObjectId id);
 }
